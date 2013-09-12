d4b0c8114b4ca6695924228a3cd25639fc73227e
Ohgyun Ahn <ohgyun@gmail.com>
2013-09-12 15:53:57 +0900

    메모리 릭 케이스 추가

--- js/code.js
+++ js/code.js
@@ -1,7 +1,17 @@
 (function () {
+    // 코드 출력
     var pre = document.createElement('pre');
     pre.classList.add('code');
     var codestr = document.getElementById('code').innerHTML;
     pre.innerHTML = codestr;
     document.body.appendChild(pre);
+
+    // 버튼명에 함수 출력
+    var buttons = document.querySelectorAll('button');
+    for (var i = 0; i < buttons.length; i++) {
+        var onclick = buttons[i].getAttribute('onclick');
+        if (onclick) {
+            buttons[i].innerHTML += ('<br><span class="func">' + onclick + '</span>');
+        }
+    }
 }());
